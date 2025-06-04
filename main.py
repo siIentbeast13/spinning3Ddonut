8ca448f23793051e20054873a55548d681a1407c
siIentbeast13 <safeermamun2023@gmail.com>
2025-06-05 03:19:18 +0600

    backup

---
+++ main.py
@@ -0,0 +1,108 @@
+import pygame
+from math import pi, sin, cos
+from random import randint
+
+
+pygame.init()
+
+
+def to_world_position(position:list[float,float]):
+    return 400+position[0], 400-position[1]
+
+
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+fov = 200
+
+
+vertices = [
+]
+
+
+i=0
+while i<5:
+    angle = 0
+    while angle < 2*pi:
+        vertices.append([(50+i*10)*sin(angle), (50+i*10)*cos(angle), i*10])
+        if i > 0:
+            vertices.append([(50+i*10)*sin(angle), (50+i*10)*cos(angle), -i*10])
+        angle += 0.05
+    i+=0.1
+
+i=0
+while i<5:
+    angle = 0
+    angle = 0
+    while angle < 2*pi:
+        vertices.append([(100+i*10)*sin(angle), (100+i*10)*cos(angle), (5-i)*10])
+        if i > 0:
+            vertices.append([(100+i*10)*sin(angle), (100+i*10)*cos(angle), -(5-i)*10])
+        angle += 0.05
+    i+=0.1
+
+def rotate_x(angle, vertices):
+    for vertex in vertices:
+        x, y, z = vertex
+        vertex[1] = y*cos(angle) - z*sin(angle)
+        vertex[2] = y*sin(angle) + z*cos(angle)
+
+def rotate_y(angle, vertices):
+    for vertex in vertices:
+        x, y, z = vertex
+        vertex[0] = x*cos(angle) + z*sin(angle)
+        vertex[2] = -x*sin(angle) + z*cos(angle)
+
+
+
+def pointVertices(vertices):
+    vertices2D = []
+    for vertex in vertices:
+        vertices2D.append((vertex[0]*fov/(vertex[2]+fov), vertex[1]*fov/(vertex[2]+fov)))
+    
+    i=0
+    for vertex in vertices2D:
+        c = max(min(-vertices[i][2]+150, 255), 0)
+        pygame.draw.circle(screen, (c,c,c), to_world_position(vertex), 3)
+        i+=1
+
+rotatingUp = False
+rotatingDown = False
+rotatingRight = False
+rotatingLeft = False
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                rotatingUp = True
+            if event.key == pygame.K_DOWN:
+                rotatingDown = True
+            if event.key == pygame.K_LEFT:
+                rotatingLeft = True
+            if event.key == pygame.K_RIGHT:
+                rotatingRight = True
+            if event.key == pygame.K_EQUALS:
+                fov -= 10
+            if event.key == pygame.K_MINUS:
+                fov += 10
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                rotatingUp = False
+            if event.key == pygame.K_DOWN:
+                rotatingDown = False
+            if event.key == pygame.K_LEFT:
+                rotatingLeft = False
+            if event.key == pygame.K_RIGHT:
+                rotatingRight = False
+    screen.fill((0,0,0))
+
+    rotate_x(0.1*(-int(rotatingUp) + int(rotatingDown)), vertices)
+    rotate_y(0.1*(int(rotatingRight) - int(rotatingLeft)), vertices)
+
+    pointVertices(vertices)
+    
+    pygame.display.flip()
+    clock.tick(60)
